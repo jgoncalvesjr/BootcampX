19307a79da454cd27eeee701d87da435c31a0e81
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-19 22:52:59 +0000

    Refactor to escape SQL injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,45 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const [, , cohort] = process.argv;
+
+const teacherQuery = () => {
+  const values = [`%${cohort || 'JUL02'}%`];
+  return pool.query(`
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN teachers ON teachers.id = teacher_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher;
+  `, values);
+};
+
+teacherQuery()
+  .then(res => {
+    console.log('Connected');
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
+
+
 // Query refactored to not need exact day of cohort
-pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-FROM assistance_requests
-JOIN students ON students.id = student_id
-JOIN teachers ON teachers.id = teacher_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-ORDER BY teacher;
-`)
-.then(res => {
-  console.log('Connected');
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+// FROM assistance_requests
+// JOIN students ON students.id = student_id
+// JOIN teachers ON teachers.id = teacher_id
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   console.log('Connected');
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// }).catch(err => console.error('query error', err.stack))
+//   .finally(() => pool.end());
